beb76591e9a4475f796a432cf00527d159a35190
akshatas28 <akshata.saptasagar@gmail.com>
2026-08-02 21:36:26 +0530

    advance python progress dict

---
+++ Lecture9/dict_store.py
@@ -0,0 +1,37 @@
+# advanced python
+
+# lect 9
+
+# store freq in dict
+
+num = [1,2,3,4,5,1,2,3,1,2,3] # dont change input
+dict1={}
+count=1
+for i in num:
+    if (i in dict1):
+        dict1[i]+=1
+    else:
+        dict1[i]=1
+print (dict1)
+
+# so TC : O(N)
+
+
+# alternative
+
+num = [1,2,3,4,5,1,2,3,1,2,3] # dont change input
+def checkfreqindict(n, dict1):
+    if (n==len(num)):
+        return dict1
+    m=num[n]
+    if (m in dict1):
+        dict1[m]+=1
+        return checkfreqindict(n+1, dict1)
+    else:
+        dict1[m]=1
+        return checkfreqindict(n+1, dict1)
+
+print(checkfreqindict(0, {}))
+
+# so TC : O(N)
+# so SC : O(k) : k is number of keys
